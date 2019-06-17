a855a5119800e00867c0ebdac1ba378333c5564f
grujicdragan <dragan_grujic93@hotmail.com>
2019-06-17 15:37:56 +0200

    colause

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VideosComponent } from './videos/videos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VideosComponent    
   ],
   imports: [
     BrowserModule,
