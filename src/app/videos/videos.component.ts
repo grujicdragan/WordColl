406275e7451610c99a820e7c939ced1cab20f986
grujicdragan <dragan_grujic93@hotmail.com>
2019-06-18 15:51:25 +0200

    dasdas

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -15,19 +15,19 @@ export class VideosComponent implements OnInit {
   }
 
   ngOnInit() {
-    var item : Item[];
-    item[0].decription="dsadasdda";
-    item[0].title="dsadadsa";
-    item[0].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
-    item[1].decription="dsadasdda";
-    item[1].title="dsadadsa";
-    item[1].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
-    item[2].decription="dsadasdda";
-    item[2].title="dsadadsa";
-    item[2].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
-    item[3].decription="dsadasdda";
-    item[3].title="dsadadsa";
-    item[3].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
+    // var item : Item[];
+    // item[0].decription="dsadasdda";
+    // item[0].title="dsadadsa";
+    // item[0].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
+    // item[1].decription="dsadasdda";
+    // item[1].title="dsadadsa";
+    // item[1].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
+    // item[2].decription="dsadasdda";
+    // item[2].title="dsadadsa";
+    // item[2].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
+    // item[3].decription="dsadasdda";
+    // item[3].title="dsadadsa";
+    // item[3].videoURL="https://www.youtube.com/embed/EL01-Xztuf8";
   }
 
 }
